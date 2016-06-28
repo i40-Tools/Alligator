57ca295a68e5761cf39dde6ee0ff500d92c71e33
Irlan <irlangrangel2003@gmail.com>
2016-06-28 16:12:02 +0200

    Including configuration from ttl file

--- resources/evalAML.pl
+++ resources/evalAML.pl
@@ -8,4 +8,4 @@
  open('d:/Deutch/development/Rules4AMLIntegration/resources/output.txt',write,FileName),
  consult("d:/Deutch/development/Rules4AMLIntegration/resources/writeRules.pl"),
  myWrite(FileName),close(FileName).
-               
+             
--- src/main/Rules4AMLIntegration.java
+++ src/main/Rules4AMLIntegration.java
@@ -2,7 +2,6 @@ package main;
 
 import java.io.IOException;
 
-import org.cs3.prolog.connector.process.PrologProcessException;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 
@@ -10,21 +9,20 @@ public class Rules4AMLIntegration {
 
 	public static void main(String[] args) {
 		Owl2PrologFactGenerator test = new Owl2PrologFactGenerator();
-		DeductiveDB deductiveDB = new DeductiveDB();
-		deductiveDB.consultKB();
-		/*try {
+		//DeductiveDB deductiveDB = new DeductiveDB();
+		//deductiveDB.consultKB();
+		try {
 			String localIRI = "d:/Deutch/development/Rules4AMLIntegration/resources/aml.ttl";
 			test.readOntology(localIRI);
 			test.generateTBoxFacts("d:/Deutch/development/Rules4AMLIntegration/resources/IntentionalDB.pl");
 			test.generateABoxFacts("d:/Deutch/development/Rules4AMLIntegration/resources/ExtensionalDB.pl");
 			
-			test.getDomainRangeAxioms();*/
 			
-		/*} catch (OWLOntologyCreationException e1) {
+		} catch (OWLOntologyCreationException e1) {
 			e1.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
+		}
 		
 	}
 
