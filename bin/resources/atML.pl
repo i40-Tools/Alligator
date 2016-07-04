99f67d418c2c3cec6e5471b7c4301d08726c771b
Irlan <irlangrangel2003@gmail.com>
2016-07-04 16:39:24 +0200

    Simple example

--- bin/resources/atML.pl
+++ bin/resources/atML.pl
@@ -15,7 +15,17 @@ clause1(refSem(a,b),true).
 clause1(refSem(c,b),true).
 clause1(refSem(c,d),true).
 clause1(refSem(h,d),true).
+clause1(refSem(h,h1),true).
+clause1(refSem(e,h1),true).
+clause1(refSem(e,h2),true). 
+clause1(refSem(e,h3),true).
+clause1(refSem(f,h3),true).
+clause1(refSem(f,h4),true).
 clause1(hasAtt(c1,c),true).
 clause1(hasAtt(c2,a),true).
+clause1(hasAtt(c1,h),true).
+clause1(hasAtt(c2,e),true).
+clause1(hasAtt(c3,c),true).
+clause1(hasAtt(c3,a),true).
 
- 
+   
