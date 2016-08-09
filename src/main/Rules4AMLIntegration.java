4233e871029bf91e8880ed792ed12443a35c9894
igrangel <irlangrangel2003@gmail.com>
2016-08-09 10:54:22 +0200

    Removing unnecesary files

--- src/main/Rules4AMLIntegration.java
+++ src/main/Rules4AMLIntegration.java
@@ -6,14 +6,6 @@ public class Rules4AMLIntegration {
 
 	public static void main(String[] args) throws Throwable {
 
-		// Owl2PrologFactGenerator test = new Owl2PrologFactGenerator();
-		// Generating Intentional DB from the Ontology
-		// test.readOntology(localIRI);
-		// test.generateIntentionalDB(conf.getFilePath() + "IntentionalDB.pl");
-
-		// Generating Extentional DB from the Ontology, if the
-		// test.generateABoxFacts(conf.getFilePath() + "ExtensionalDB.pl");
-
 		// Generating facts from the AML files, they are converted into RDF
 		Files2Facts filesAMLInRDF = new Files2Facts();
 		try {
